26b7b61eb1a45b8fedad6161d8fe5f5fbbcad6a1
Vanek <zatkinivan73@gmail.com>
2026-09-16 13:55:58 +0300

    решил пробелму , теперь через сайт можно добавлять задачи

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
 
 def task_list(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+            return redirect('task_list')
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
